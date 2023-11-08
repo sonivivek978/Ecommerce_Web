cf21abd5846bdc9fc2cdce87a3e3e92fa6704389
vivek <sonivivek978@gmail.com>
2023-11-08 17:25:28 +0530

    Revert "Revert "add latest server code""
    This reverts commit 1cffd8f3595438f9dd700f365df1fcddcb107e6f.

--- server/server/routes/CartRouter.js
+++ server/server/routes/CartRouter.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { AddCart, GetCart } = require("../controllers/CartControllers");
+const {
+  AddCart,
+  GetCart,
+  DeleteCart,
+} = require("../controllers/CartControllers");
 
 router.post("/vs/cart/:userId/product/:productId", AddCart);
-router.get("/vs/cart/:id",GetCart);
+router.get("/vs/cart/:id", GetCart);
+router.delete("/vs/cart/:userId/product/:productId", DeleteCart);
 
 module.exports = router;
